39dd437bdab5f1ea2f28a0eaad30d439a30108b5
eHijano <31777294+ehijano@users.noreply.github.com>
2020-10-15 10:42:34 -0700

    Add files via upload

---
+++ Source_code/utils/LengthRestrictedDocument.java
@@ -0,0 +1,28 @@
+package utils;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+public final class LengthRestrictedDocument extends PlainDocument {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+private final int lim;
+
+  public LengthRestrictedDocument(int lim) {
+    this.lim = lim;
+  }
+
+  @Override
+  public void insertString(int offs, String str, AttributeSet a)
+      throws BadLocationException {
+    if (str == null)
+      return;
+
+    if ((getLength() + str.length()) < lim) {
+      super.insertString(offs, str, a);
+    }
+  }
+}
